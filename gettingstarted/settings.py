90ac34b1142227cc376b9fa6b8e69cfa2e947ae1
Amar Prakash Pandey <amar.om1994@gmail.com>
2016-11-11 22:50:56 +0530

    check

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -36,8 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'hello'
+    'django.contrib.staticfiles'
 )
 
 MIDDLEWARE_CLASSES = (
