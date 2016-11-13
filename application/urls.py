c6bdc660a5c3dbaa48d70008da361fcdf5e7ce6d
Amar Prakash Pandey <amar.om1994@gmail.com>
2016-11-13 18:53:30 +0530

    django form created

--- application/urls.py
+++ application/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name="index"),
-	url(r'^stats/$', views.stats, name="stats")
+	url(r'^stats/$', views.stats, name="stats"),
+	url(r'^stats/(?P<username>[a-z]\w+)/$', views.username, name="username")
 ]
--- application/views.py
+++ application/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django import forms
+
+class NameForm(forms.Form):
+    username = forms.CharField(
+    	widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder' : 'GitHub username'}),
+    	label='', 
+    	max_length=200
+    )
 
 # Create your views here.
 def index(request):
-	return render(request, 'application/index.html')
+	if request.method == 'POST':
+		form = NameForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data['username']
+			return HttpResponseRedirect('stats/'+ username)
+	else:
+		form = NameForm()
+	return render(request, 'application/index.html', {'form': form})
 
 def stats(request):
-	return render(request, 'application/stats.html')
+	return redirect("/")
+
+def username(request, username):
+	context = {
+		'username' : username
+	}
+	return render(request, 'application/stats.html', context)
